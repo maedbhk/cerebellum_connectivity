d13b0c57abe3f8b8fc4f395bcf9f87f8eb498a54
Maedbh King <maedbhking@berkeley.edu>
2019-08-26 16:55:43 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='connectivity',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Modelling cerebro-cerebellar connectivity using MDTB data',
+    author='Maedbh King',
+    license='MIT',
+)
