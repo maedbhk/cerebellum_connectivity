51594ab0d3ec98e14edc46dfbc8fdacd3b2d494e
Maedbh King <mking86@uwo.ca>
2020-08-11 21:15:34 -0700

    added doc strings to train_model

--- connectivity/models/train_model.py
+++ connectivity/models/train_model.py
@@ -27,19 +27,36 @@ from connectivity.data.prep_data import DataManager
 from connectivity.constants import Defaults
 
 class TrainModel(DataManager):
+    """ Model fitting Class 
+        Model specifications are set in Class __init__
+    """
 
-    def __init__(self, model="plsregress"):
+    def __init__(self, model = "plsregress"):
+        """ Initialises inputs for TrainModel Class
+            Args: 
+                model (str): model name default is "plsregress"
+            
+            __init__:
+            params (np array): model parameters # TBD
+            glm (int): glm number: default is 7. options are 7 and 8
+            experiment (list of str): study to be used for training. options are 'sc1' or 'sc2'
+            model_inputs (nested dict): primary keys are `X` and `Y` 
+            train_mode (str): training mode: 'crossed' or 'uncrossed'. If 'crossed': sessions are flipped between `X` and `Y`
+            incl_instr (bool): include instructions in model fitting. default is True
+            avg (str): average data across runs within a session or across session. options are 'run' and 'session'
+            substract_sess_mean (bool): subract session mean. default is True
+            subtract_exp_mean (bool): subtract experiment mean. default is False
+        """
         super().__init__()
         self.model = model
         self.params = None
         self.glm = 7
-        # self.rois = {'cortex':'tesselsWB162', 'cerebellum':'grey_nan'} 
-        self.experiment = ['sc1']# 'sc1' or 'sc2' # was `train_experiment`
+        self.experiment = ['sc1']
         self.model_inputs = {'X': {'roi': 'tesselsWB162', 'file_dir': 'encoding'},
                              'Y': {'roi': 'grey_nan', 'file_dir': 'encoding'},
                             }
         self.train_mode = 'crossed'
-        self.inclInstr = True # True or False
+        self.inclInstr = True
         # self.scale = True
         self.overwrite = True
         self.avg = 'run' # 'run' or 'sess'
@@ -47,6 +64,8 @@ class TrainModel(DataManager):
         self.subtract_exp_mean = False # not yet implemented
 
     def _get_model_data(self):
+        """ returns model data based on `model_inputs` set in __init__
+        """
         # get model data: `X` and `Y` based on `model_inputs`
         model_data = {}
         for input in self.model_inputs:
@@ -57,7 +76,9 @@ class TrainModel(DataManager):
         return model_data
     
     def model_fit(self):
-        
+        """ Model fitting routine on individual subject data, saved to disk
+        """
+
         # get model data: `X` and `Y` based on `model_inputs`
         model_data = self._get_model_data()
 
