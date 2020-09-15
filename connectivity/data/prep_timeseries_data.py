8892b9b1373e1278ddaef300c35851b4d5295e3f
Amanda <amanda.irene.lebel@gmail.com>
2020-09-15 15:41:00 -0700

    add masking functions for imaging data

--- connectivity/data/prep_timeseries_data.py
+++ connectivity/data/prep_timeseries_data.py
@@ -68,7 +68,7 @@ class DataManager:
         """
         roi = self.data_dtype['roi']
         if roi == 'voxelwise':
-            fname = f'rrun_%s.nii'
+            fname = f's{sub}rrun_%s.nii'
         if self.data_type['file_dir'] == 'imaging_data':
             fpath = os.path.join(self.dirs.IMAGING_DIR, fname)
         return fpath
@@ -77,7 +77,7 @@ class DataManager:
     def _concat_exps(self):
         """ retrieves data:
         Returns:
-            T_concat(nested dict): keys are exp
+            T_concat(dict): keys are exp - values are data in shape (time, x,y,z (48, 84, 84))
         """
         
         T_concat = {}
@@ -94,15 +94,53 @@ class DataManager:
                 runs = list(range(1, 16, 1))
             elif exp == 'sc2':
                 runs = list(range(16, 33, 1))
-            for run in runs:
-                
-                        
             # load imaging data from nii
+            data_runs = []
+            for run in runs:
+                data_runs.append(nib.load(fpath%(run)).get_data().T)
+            T_concat[exp] = np.concatenate
             
         return T_concat
     
     def _get_masks(self):
-        pass
+        """ return cerebellar and cortical masks for each subject minus the buffer voxels.
+        
+        Returns:
+           masks (nested dict): keys are subjects followed by keys as "cerebellum" and "cortex"
+        """
+        
+        masks = dict()
+        self.dirs = Dirs(study_name=self.exp, glm=self.glm)
+        for self.subj in self.subjects:
+            individ_masks = dict()
+            fname = f'{self.subj}/maskbrainSUITGrey.nii
+            fpath = os.path.join(self.dirs.SUIT_ANAT_DIR, fname)
+            
+            cerebellar = nib.load(fpath).get_data().T
+            
+            fname = f'{self.subj}/rmask_gray.nii'
+            fpath = os.path.join(self.dirs.IMAGING_DIR, fname)
+            cortex = nib.load(fpath).get_data().T
+            
+            fname = f'{self.subj}/buffer_voxels.nii'
+            fpath = os.path.join(self.dirs.SUIT_ANAT_DIR, fname)
+            buffer = nib.load(fpath).get_data().T
+            
+            cerebellar[buffer!=0]=0
+            cerebellar[cerebellar!=0]=1
+            cortex[buffer!=0]=0
+            cortex[cerebellar!=0]=0
+            cortex[cortex!=0]=1
+            
+            individ_masks['cerebellum'] = cerebellar.astype('bool')
+            individ_masks['cortex'] = cortex,astype('bool')
+            
+            masks[f'{self.subj}'] = individ_masks
+         
+        
+        return masks
+        
+            
     
     
         
