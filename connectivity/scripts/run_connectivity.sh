0aa72eab8f347420a5d77822a03d68e8b193c5ec
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2020-12-10 13:09:46 -0500

    Restructing of Dirs in repro

--- connectivity/scripts/run_connectivity.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. /Users/maedbhking/.local/share/virtualenvs/cerebellum_connectivity-DQHkR575/bin/activate
-cd /Users/maedbhking/Documents/cerebellum_connectivity/connectivity/models
-
-python3 run_connectivity.py train=True evaluate=True train_on='"sc1"' eval_on='"sc2"' train_X_roi='"tesselsWB162"' eval_X_roi='"tesselsWB162"' lambdas='[0,10,20,50,100,200,500,1000]'
