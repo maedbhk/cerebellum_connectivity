d0db247752d8f8de5d75f7026835b7f9af243bbf
Maedbh King <mking86@uwo.ca>
2021-03-17 10:04:52 -0700

    modified data_transfer.py

--- connectivity/savio_scripts/data_transfer.py
+++ connectivity/savio_scripts/data_transfer.py
@@ -2,12 +2,11 @@ import os
 
 def from_savio():
     # transfer eval data from savio to local
-    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/glm7/eval/* /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/glm7/eval/")
-    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/glm7/eval/* /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/conn_models/glm7/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/ /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/eval/ /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/conn_models/eval/")
 
-    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc1/encoding/glm7/grey_nan_nonZeroInd.json /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/encoding/glm7/")
-    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc2/encoding/glm7/grey_nan_nonZeroInd.json /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/encoding/glm7/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/train/train_summary.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/train/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/train/train_summary.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/conn_models/train/")
 
 def to_savio():
-    # transfer bash script
-    os.system("rsync -avrz /Users/maedbhking/Documents/cerebellum_connectivity/connectivity/scripts/run_savio_jobs.sh maedbhking@dtn.brc.berkeley.edu:/global/scratch/maedbhking/projects/cerebellum_connectivity/data/savio_jobs/")
+    pass
