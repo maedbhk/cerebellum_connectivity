7209295ca8a7ea5d7a3d4340f28c22ee58050622
Maedbh King <mking86@uwo.ca>
2021-04-07 15:41:44 -0700

    modified run_connectivity_Train

--- connectivity/savio_scripts/run_connectivity_train.sh
+++ connectivity/savio_scripts/run_connectivity_train.sh
@@ -24,14 +24,8 @@ export PYTHONPATH
 cd /global/scratch/maedbhking/projects/cerebellum_connectivity/connectivity/scripts
 
 # run connectivity models
-python3 script_mk.py --cortex="tesselsWB162" --model_type="ridge" --train_or_eval="train"
-python3 script_mk.py --cortex="tesselsWB362" --model_type="ridge" --train_or_eval="train"
-python3 script_mk.py --cortex="tesselsWB642" --model_type="ridge" --train_or_eval="train"
-
-python3 script_mk.py --cortex="tesselsWB162" --model_type="WTA" --train_or_eval="train"
-python3 script_mk.py --cortex="tesselsWB362" --model_type="WTA" --train_or_eval="train"
-python3 script_mk.py --cortex="tesselsWB642" --model_type="WTA" --train_or_eval="train"
-
-# python3 script_mk.py --cortex="tesselsWB162" --model_type="NNLS" --train_or_eval="train"
-# python3 script_mk.py --cortex="tesselsWB362" --model_type="NNLS" --train_or_eval="train"
-# python3 script_mk.py --cortex="tesselsWB642" --model_type="NNLS" --train_or_eval="train"
+python3 script_mk.py --cortex="tessels0042" --model_type="WTA" --train_or_eval="train"
+# python3 script_mk.py --cortex="tessels0162" --model_type="WTA" --train_or_eval="train"
+# python3 script_mk.py --cortex="tessels0362" --model_type="WTA" --train_or_eval="train"
+# python3 script_mk.py --cortex="tessels0642" --model_type="WTA" --train_or_eval="train"
+# python3 script_mk.py --cortex="tessels1002" --model_type="WTA" --train_or_eval="train"
