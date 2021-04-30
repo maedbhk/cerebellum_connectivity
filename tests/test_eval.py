449fe462bb51c6740d15a9e38d2f37a9bc6df723
Maedbh King <mking86@uwo.ca>
2021-04-29 18:52:53 -0700

    add test for eval

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -9,15 +9,18 @@ models = ['ridge_yeo7_alpha_2', 'ridge_yeo17_alpha_2',
 
 for exp in range(2):
 
-    for model in models:
+    try: 
+        for model in models:
 
-        cortex = model.split('_')[1]
+            cortex = model.split('_')[1]
 
-        # save voxel/vertex maps for best training weights
-        script_mk.save_weight_maps(model_name=model, cortex=cortex, train_exp=f"sc{2-exp}")
+            # save voxel/vertex maps for best training weights
+            script_mk.save_weight_maps(model_name=model, cortex=cortex, train_exp=f"sc{2-exp}")
 
-        # test best train model
-        script_mk.eval_model(model_name=model, cortex=cortex, train_exp=f"sc{2-exp}", eval_exp=f"sc{exp+1}")
+            # test best train model
+            script_mk.eval_model(model_name=model, cortex=cortex, train_exp=f"sc{2-exp}", eval_exp=f"sc{exp+1}")
+    except: 
+        pass
 
 if __name__ == "__main__":
     run()
