0137e3715054a7f299135ea5aa4a4c5485094e3b
Maedbh King <mking86@uwo.ca>
2021-10-19 20:48:36 -0700

    fixed bug in cortical_surface

--- connectivity/savio_scripts/run_weight_maps.sh
+++ connectivity/savio_scripts/run_weight_maps.sh
@@ -25,14 +25,7 @@ export PYTHONPATH
 cd /global/scratch/users/maedbhking/projects/cerebellum_connectivity/connectivity/scripts
 
 # run weight maps
-atlases=(MDTB10)
-weights=(positive absolute)
-data_type=(label func)
-
-for ((w=0; w<${#weights[@]}; w++)); do \ 
-    for ((a=0; a<${#atlases[@]}; a++)); do \
-        for ((b=0; b<${#data_type[@]}; b++)); do \
-            python3 script_weight_maps.py --atlas=${atlases[a]} --weights=${weights[w]} --data_type=${data_type[b]}; done; done; done
+python3 script_cortical_surface.py
 
 # connect_dir=/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/train/best_weights
 # learn_dir=/global/scratch/users/maedbhking/projects/cerebellum_learning_connect/data/BIDS_dir/derivatives/conn_models/train
