8d4307b4b64e611fb8e0704a85fcaa60b0ce77c6
Maedbh King <mking86@uwo.ca>
2021-11-10 18:05:32 +0000

    add exclude_instruct

--- connectivity/scripts/data_transfer.py
+++ connectivity/scripts/data_transfer.py
@@ -3,6 +3,8 @@ import os
 def from_savio():
     #transfer eval data from savio to local
     os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/eval_summary_mk1.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/eval_summary_mk2.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/eval_summary_mk.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
    
     # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/train/*.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/conn_models/train/")
 
