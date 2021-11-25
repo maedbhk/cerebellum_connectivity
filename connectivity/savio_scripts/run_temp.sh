a76db6473fef2947c694c5b2fa679aad9aa6b371
Maedbh King <mking86@uwo.ca>
2021-11-25 12:00:18 +0000

    add temp bash script

---
+++ connectivity/savio_scripts/run_temp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=temp
+#
+# Account:
+#SBATCH --account=fc_cerebellum
+#
+# Partition:
+#SBATCH --partition=savio2
+
+# Quality of Service:
+#SBATCH --qos=savio_normal
+#
+# Wall clock limit:
+#SBATCH --time=01:00:00
+#
+## Command(s) to run:
+module load python/3.7
+source ~/.bash_profile
+source $(pipenv --venv)/bin/activate
+
+PYTHONPATH=$PYTHONPATH:/global/scratch/users/maedbhking/bin/SUITPy/
+export PYTHONPATH
+
+cd /global/scratch/users/maedbhking/projects/cerebellum_connectivity/connectivity/scripts
+
+# transfer best weights
+connect_dir=/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/train/best_weights
+learn_dir=/global/scratch/users/maedbhking/projects/cerebellum_learning_connect/data/BIDS_dir/derivatives/conn_models/train
+
+python3 run_transfer_weights.py --connect_dir=${connect_dir} --learn_dir=${learn_dir}
