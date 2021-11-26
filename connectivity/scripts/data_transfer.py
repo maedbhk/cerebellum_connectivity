b5240708e84156bc32617b2a19e39b7685cac19a
Maedbh King <mking86@uwo.ca>
2021-11-26 10:39:26 +0000

    fixed bug in run_transfer_weights

--- connectivity/scripts/data_transfer.py
+++ connectivity/scripts/data_transfer.py
@@ -4,7 +4,7 @@ def from_savio():
     #transfer eval data from savio to local
     os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/eval_summary_mk.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
    
-    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/train/*.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/conn_models/train/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/train/*.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/conn_models/train/")
 
 def to_savio():
     pass
