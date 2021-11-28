f34a3a941d4b5ca78230743b2caebb11fd4fcaae
Maedbh King <mking86@uwo.ca>
2021-11-28 09:35:28 +0000

    modified best_weights script

--- connectivity/savio_scripts/run_convergence.sh
+++ connectivity/savio_scripts/run_convergence.sh
@@ -25,18 +25,18 @@ export PYTHONPATH
 cd /global/scratch/users/maedbhking/projects/cerebellum_connectivity/connectivity/scripts
 
 # # run cortical surface (voxels)
-# python3 script_surfaces.py --exp="sc1" --weights="nonzero" --method="lasso" --regions="voxels"
+python3 script_surfaces.py --exp="sc1" --weights="nonzero" --method="lasso" --regions="voxels"
 
 atlases=(MDTB10) # Buckner7 Buckner17 Anatom (problem with these two atlases)
 
 # run cortical surfaces (rois)
-# for ((a=0; a<${#atlases[@]}; a++)); do \
-# python3 script_surfaces.py --exp="sc1" --weights="nonzero" --method="lasso" --regions="rois" --atlas=${atlases[a]}; done
+for ((a=0; a<${#atlases[@]}; a++)); do \
+python3 script_surfaces.py --exp="sc1" --weights="nonzero" --method="lasso" --regions="rois" --atlas=${atlases[a]}; done
 
 # # run dispersion
 for ((a=0; a<${#atlases[@]}; a++)); do \
 python3 script_dispersion.py --atlas=${atlases[a]} --method="ridge" --exp="sc1"; done
 
 # cortical weights
-# for ((a=0; a<${#atlases[@]}; a++)); do \
-# python3 script_cortical_weights.py --atlas=${atlases[a]} --method="ridge" --exp="sc1"; done
+for ((a=0; a<${#atlases[@]}; a++)); do \
+python3 script_cortical_weights.py --atlas=${atlases[a]} --method="ridge" --exp="sc1"; done
