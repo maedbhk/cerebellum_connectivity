524b3f3f378d3c036725f36aea30d1b95dae183d
Maedbh King <mking86@uwo.ca>
2021-12-06 13:22:32 +0000

    fixed bug in convergnce

--- connectivity/scripts/data_transfer.py
+++ connectivity/scripts/data_transfer.py
@@ -2,12 +2,16 @@ import os
 
 def from_savio():
     # #transfer eval data from savio to local
-    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/eval_summary_mk.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/*.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
    
     # # transfer best models
-    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/ridge_tessels1002_alpha_8 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
-    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/lasso_tessels1002_alpha_-2 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
-    # os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/WTA_tessels1002 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/ridge_tessels1002_alpha_8 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/lasso_tessels1002_alpha_-2 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/WTA_tessels1002 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/WTA_tessels0042 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc2/conn_models/eval/WTA_yeo7 /Users/maedbhking/Documents/cerebellum_connectivity/data/sc2/conn_models/eval/")
+    
 
     #transfer train data
     os.system("rsync -avrz maedbhking@dtn.brc.berkeley.edu:/global/scratch/users/maedbhking/projects/cerebellum_connectivity/data/sc1/conn_models/train/*.csv /Users/maedbhking/Documents/cerebellum_connectivity/data/sc1/conn_models/train/")
