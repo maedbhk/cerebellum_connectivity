d3808d93125104e59d2a17848abd981a282efc13
Maedbh King <mking86@uwo.ca>
2022-03-03 17:49:09 -0800

    remove mdtb from best_weights

--- connectivity/savio_scripts/run_best_weights.sh
+++ connectivity/savio_scripts/run_best_weights.sh
@@ -25,7 +25,7 @@ export PYTHONPATH
 cd /global/scratch/users/maedbhking/projects/cerebellum_connectivity/connectivity/scripts
 
 # get best weights
-methods=(WTA) # ridge lasso
+methods=(ridge lasso WTA) # ridge lasso
 for ((m=0; m<${#methods[@]}; m++)); do \
 python3 script_best_weights.py --exp="sc1" --method=${methods[m]}; done
 
